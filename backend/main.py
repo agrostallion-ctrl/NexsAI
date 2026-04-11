9243eaf5f6dbab12bafa7b93adeafd4989a00208
Nitish Kumar <agrostallion@gmail.com>
2026-04-11 17:15:52 +0530

    fix cors for vercel

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ origins = [
     "http://localhost:3001",
     "http://127.0.0.1:3001",
     "https://nexs-ai.vercel.app",  # 👈 important
+    "https://nexs-ai.vercel.app"
 ]
 
 app.add_middleware(
